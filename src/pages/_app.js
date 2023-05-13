50a5a23d2e04d49d24705204b2b2f6fb0b14dba7
zaryabzaheer8888 <101867054+zaryabzaheer8888@users.noreply.github.com>
2023-05-13 18:38:11 +0500

    my first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+// src/pages/_app.js
+
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../../prismicio'
+
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
---
+++ src/pages/slice-simulator.js
@@ -0,0 +1,17 @@
+// /pages/slice-simulator.jsx
+
+import { SliceSimulator } from "@prismicio/slice-simulator-react";
+import { SliceZone } from "@prismicio/react";
+
+import { components } from "../slices";
+
+const SliceSimulatorPage = () => (
+  <SliceSimulator
+    sliceZone={({ slices }) => (
+      <SliceZone slices={slices} components={components} />
+    )}
+    state={{}}
+  />
+);
+
+export default SliceSimulatorPage;
